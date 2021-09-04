e4d5e720e2314ebb9541678a66308bfbc2173b90
Ricardo Costa <ricardo18acs@gmail.com>
2021-09-04 16:50:45 -0300

    Test request

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
+const { promisify } = require('util');
+const req = require('request');
+const request = promisify(req);
+
 module.exports = class Application {
-    init() {
-        console.log('Init application...')
+    async init() {
+        try {
+
+            // const { stdout, stderr } = await exec("node ");
+            const resp = await request('https://6133c9827859e700176a37ab.mockapi.io/api/v1/delivery/up', {
+                id: 120
+            });
+
+            if (resp.body) {
+                console.log('Event: Ok, 200...')
+            }
+
+            return;
+
+        } catch (e) {
+            console.error(e);
+        }
     }
 }
---
+++ src/run.js
@@ -0,0 +1,5 @@
+const Application = require("./app");
+
+const app = new Application();
+
+app.init();
