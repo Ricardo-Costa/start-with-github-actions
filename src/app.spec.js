0b7930f3f164c9b53c1ee662b96efc2557d95f5c
Ricardo Costa <ricardo18acs@gmail.com>
2021-09-04 15:37:01 -0300

    Test changes

--- src/app.spec.js
+++ src/app.spec.js
@@ -2,4 +2,8 @@ describe('App', () => {
     it('Should just run an example.', () => {
         expect(true).toBe(true);
     })
+
+    it('Just to update my file.', () => {
+        expect(true).toBe(true);
+    })
 })
